120b5d31f831971904e684f09c411dc5bc206039
Collin <collin.d.snyder@gmail.com>
2018-07-06 14:02:43 -0400

    Going to try to create the mobile menu with its own box outsize the list frame. Desperate measures testing

---
+++ js/mobile.js
@@ -0,0 +1,7 @@
+var mobileButton = document.querySelector(".nav-mobile-button");
+var mobileMenu = document.querySelector(".nav-mobile");
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("is-hidden");
+    //mobileMenu.classList.toggle("nav-mobile-transition");
+}
