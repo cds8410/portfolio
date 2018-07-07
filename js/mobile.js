2f8880956686766c1658d70a2a020115f800a73f
Collin <collin.d.snyder@gmail.com>
2018-07-07 11:28:06 -0400

    Mobile Menu has been fixed. Main content of the sites about to be added.

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,9 @@
-var mobileButton = document.querySelector(".nav-mobile-button");
-var mobileMenu = document.querySelector(".nav-mobile");
+var mobileButton = document.querySelector(".nav-mobile-icon");
+var mobileMenu = document.querySelector(".nav-mobile-menu");
+var shadow = document.querySelector(".portfolio-overlay");
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle("is-hidden");
+    shadow.classList.toggle("is-hidden");
     //mobileMenu.classList.toggle("nav-mobile-transition");
 }
